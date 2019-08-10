edc6f8288d724687e615963cd09e2173bbdfb104
honeywine <joeplatina78@gmail.com>
2019-08-10 16:37:28 -0400

    08-10-2019

--- src/main/python/com/revature/main.py
+++ src/main/python/com/revature/main.py
@@ -1,10 +1,47 @@
-#!/usr/bin/env python3
+'''
+This is the main script.
+'''
 
-'''
-This is your main script, this should call several other scripts within your packages.
-'''
+## imports
+import sys, os
+sys.path.append("/".join(sys.path[0].split('/')[:-1]))
+
+from PyInquirer import prompt
+from revature.controller import banking
+# from revature.controller import router
+
+
+## global variables
+_question1 = {
+	'type': 'list',
+	'name': 'q1',
+	'message': 'Welcome to Revature GCP.  What do you want to do?',
+	'choices': [
+		'Routing',
+		'Banking',
+		'exit',
+	],
+}
+
+
+## functions
 def main():
-	print('TO-DO')
+	"""
+	Main console.
+	"""
+	while(True):
+		answer = prompt(_question1)
+		if (answer['q1'] == 'exit'):
+			print("Goodbye...")
+			exit()
+		elif (answer['q1'] == 'Routing'):
+			## tentative...
+			print("In development...")
+			exit()
+		else:
+			banking.console()
 
+
+## main routine
 if __name__ == '__main__':
 	main()
